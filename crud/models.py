a8abbc7adfd8b1b7148b1cf5af3740e3f45cb3aa
malik <bittiev.malik@mail.ru>
2023-04-05 10:57:38 +0300

    first fix

--- crud/models.py
+++ crud/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Question (models.Model):
-    id = models.IntegerField(blank=True)
-    course_id =  models.IntegerField()
+    id = models.IntegerField(primary_key=True)
+    course_id =  models.IntegerField(default=0)
     question = models.CharField(max_length=255)
     answer = models.TextField()
     class Meta:
@@ -12,7 +12,7 @@ class Question (models.Model):
         return self.question    
  
 class Course (models.Model):
-    id = models.IntegerField
+    id = models.IntegerField(primary_key=True)
     name = models.TextField
     class Meta:
         db_table = 'courses'
